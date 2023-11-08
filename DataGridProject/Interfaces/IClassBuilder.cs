cf7111f992c153f74ecc24205bb1b662902dae70
Pbottie <pbottie@gmail.com>
2023-11-08 09:44:15 +0100

    Moved some more

---
+++ DataGridProject/Interfaces/IClassBuilder.cs
@@ -0,0 +1,16 @@
+﻿namespace DataGridProject.Interfaces;
+
+public interface IClassBuilder
+{
+    ClassBuilder Begin(string value);
+    ClassBuilder Begin(string value, bool condition);
+    ClassBuilder Begin(string prefix, string value);
+    ClassBuilder Begin(string prefix, string value, bool condition);
+    ClassBuilder AddClass(string value);
+    ClassBuilder AddClass(string value, bool condition);
+    ClassBuilder AddClass(string prefix, string value);
+    ClassBuilder AddClass(string prefix, string value, bool condition);
+    ClassBuilder AddClass(IEnumerable<string> values);
+    ClassBuilder AddClassFromAttributes(IReadOnlyDictionary<string, object> additionalAttributes);
+    string GetClass();
+}
