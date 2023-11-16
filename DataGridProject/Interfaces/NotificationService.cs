63c353af2d7ff0edb7ffbaaea91628df66be1efa
Pbottie <pbottie@gmail.com>
2023-11-16 16:54:02 +0100

    Added Notification and Pages

---
+++ DataGridProject/Interfaces/NotificationService.cs
@@ -0,0 +1,41 @@
+﻿namespace DataGridProject.Interfaces;
+
+public class NotificationService
+{
+    public event Action<string, NotificationType, int> OnNotify;
+
+    public void Notify(string message, NotificationType notificationType, int delay = 2500)
+    {
+        OnNotify?.Invoke(message, notificationType, delay);
+    }
+
+}
+
+public enum NotificationType
+{
+    None,
+    Success,
+    Error,
+    Info,
+    Warning,
+    Primary,
+}
+public static class TimeoutHelper
+{
+    public static CancellationTokenSource SetTimeout(Action action, int timeout)
+    {
+        var cts = new CancellationTokenSource();
+        var ct = cts.Token;
+        Task.Delay(timeout).ContinueWith((Task task) =>
+        {
+            if (!ct.IsCancellationRequested)
+            {
+                action();
+            }
+        }, ct);
+        return cts;
+    }
+
+    public static void ClearTimeout(CancellationTokenSource cts) => cts?.Cancel();
+
+}
