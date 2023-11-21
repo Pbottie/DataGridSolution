fc3ab5b38e8d098d864b2dee409791757e2b9a68
Pbottie <pbottie@gmail.com>
2023-11-21 15:56:53 +0100

    Added Interface to NotificationService removed a ToList

--- DataGridProject/Client/Program.cs
+++ DataGridProject/Client/Program.cs
@@ -16,7 +16,7 @@ public class Program
 
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
         builder.Services.AddSingleton<IFilter,FilterService>();
-        builder.Services.AddSingleton<NotificationService>();
+        builder.Services.AddSingleton<INotificationService,NotificationService>();
 
         await builder.Build().RunAsync();
     }
--- DataGridProject/Components/Notifications/NotificationService.cs
+++ DataGridProject/Components/Notifications/NotificationService.cs
@@ -1,6 +1,18 @@
 ﻿namespace DataGridProject.Components;
 
-public class NotificationService
+public interface INotificationService
+{
+    event Action<string, NotificationType, int> OnNotify;
+
+    void Notify(string message, NotificationType notificationType, int delay = 5000);
+    void Success(string message, int delay = 5000);
+    void Error(string message, int delay = 5000);
+    void Info(string message, int delay = 5000);
+    void Warning(string message, int delay = 5000);
+    void Primary(string message, int delay = 5000);
+}
+
+public class NotificationService : INotificationService
 {
     public event Action<string, NotificationType, int> OnNotify;
 
@@ -9,6 +21,30 @@ public class NotificationService
         OnNotify?.Invoke(message, notificationType, delay);
     }
 
+    public void Success(string message, int delay = 5000)
+    {
+        Notify(message, NotificationType.Success, delay);
+    }
+
+    public void Error(string message, int delay = 5000)
+    {
+        Notify(message, NotificationType.Error, delay);
+    }
+
+    public void Info(string message, int delay = 5000)
+    {
+        Notify(message, NotificationType.Info, delay);
+    }
+
+    public void Warning(string message, int delay = 5000)
+    {
+        Notify(message, NotificationType.Warning, delay);
+    }
+
+    public void Primary(string message, int delay = 5000)
+    {
+        Notify(message, NotificationType.Primary, delay);
+    }
 }
 
 public enum NotificationType
